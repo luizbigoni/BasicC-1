d468502bb9446fdcc3fc111feecc86bbae80304a
VitorHSR18 <136871451+VitorHSR18@users.noreply.github.com>
2023-08-29 09:43:18 -0300

    Update Funçoes.cpp

--- Funçoes.cpp
+++ Funçoes.cpp
@@ -3,12 +3,14 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<math.h>
+
 //uma função que que usa outra função precisas enxergar ela...
 //declaraçoão de funções
 int inttobin(int decimal);//tenho que declarar esse int se ele estiver antes do int main, para o programa saber que ele esta em algum lugar. para isso usa o ";"
 int levalor(void);
 void exibevalor(int n);
 int inttodec(int bin);
+void NumPerfeitos(int num);
 
 /* Converter decimal para binario
 int main(void)
@@ -20,18 +22,23 @@ int main(void)
 	return 0;
 }
 */
-
+/* Converter Binario Para decimal
 int main(void){
 	int num=levalor();
 	num=inttodec(num);
 	exibevalor(num);
+}
+*/
+int main(void){
+	int num=levalor();
+	NumPerfeitos(num);
 }
 
 int levalor(void)//quando n é void precisa do return-alguma coisa
 {
 	int valor;
 	//printf("digite o numero inteiro em decimal: ");
-	printf("digite o numero inteiro em binario: ");
+	printf("\ndigite o numero inteiro: ");
 	scanf("%d", &valor);
 	return valor;
 	
@@ -43,7 +50,7 @@ void exibevalor(int n)//quando é void n precisa de return
 	getch();
 }
 
-
+//Decimal em binario
 int inttobin(int decimal)
 {
 	int binario=0, soma;
@@ -55,6 +62,7 @@ int inttobin(int decimal)
 	}	
 	return binario;
 }
+//Binario em decimal
 int inttodec (int bin)
 {
 	int dec=0, resto,soma;
@@ -68,4 +76,27 @@ int inttodec (int bin)
 	
 	
 	return dec;
+}
+
+//Numeros perfeitos
+void NumPerfeitos(int num)
+{
+ int soma=0,i,aux = num;
+ 
+ for (i=1;i!=aux/2+1;i++)
+ {
+    if(num%i==0)
+    {
+        soma+= i;
+    }
+    
+ }
+    if(soma == aux)
+    {
+        printf("\nO numero é perfeito!");
+    }
+    else{
+        printf("\nO numero é imperfeito");
+    }
+    
 }
