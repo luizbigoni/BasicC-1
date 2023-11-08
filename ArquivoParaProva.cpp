f5be962502b36a77fd3eb50f2e3e34c8fbd72599
VitorHSR18 <136871451+VitorHSR18@users.noreply.github.com>
2023-11-08 20:01:32 -0300

    Update ArquivoParaProva.cpp

--- ArquivoParaProva.cpp
+++ ArquivoParaProva.cpp
@@ -1,285 +1,277 @@
+// Arquivos Binários
+
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
-#include <conio2.h>
 #include <stdlib.h>
-struct tp_func{
-	int matricula;
-	float salario;
-	char nome[30];
+#include <conio2.h>
+#include <ctype.h>
+
+struct TpFunc
+{
+	int Matricula;
+	float Salario;
+	char Nome[30];
 };
 
-int BuscaFunc(FILE *Ptr,int matricula){
-	tp_func registro;
-	rewind(Ptr); //volta ponteiro ao inicio		 ou 	fseek(Ptr,0,0); -> para começar do inicio
+int BuscaFunc(FILE *Ptr, int Mat)
+{
+	TpFunc R;
+	rewind(Ptr); //fseek(Ptr,0,0);
 	
-	fread(&registro,sizeof(tp_func),1,Ptr); //entra lido
-	while(!feof(Ptr) && matricula!=registro.matricula)
-		fread(&registro,sizeof(tp_func),1,Ptr); //sai lido
-
-	if(!feof(Ptr))//verifica se nao ta no final do arquivo ou 		/// if(matricula==registro.matricula)
-		return ftell(Ptr); /// retorna a posição do ponteiro atualmente (posição - tamanho do byte)
-
+	fread(&R,sizeof(TpFunc),1,Ptr);
+	while (!feof(Ptr) && Mat!=R.Matricula)
+			fread(&R,sizeof(TpFunc),1,Ptr);
+	
+	if (Mat==R.Matricula) //!feof(Ptr))
+		return ftell(Ptr)-sizeof(TpFunc);
 	else
 		return -1;
 }
-void exibir (void){
-	tp_func f;
-	int i;
-		FILE *Ptr =fopen("func.dat","rb");		
-		if(Ptr==NULL) ///verifica se o arquivo existe
-			printf("\nErro de abertura!\n");
-		
-		else{
-			fread(&f,sizeof(tp_func),1,Ptr);//faz um entra lido e sai lido para nao repetir no final a mesma informação
-			while(!feof(Ptr)){
-					printf("\nmatricula: [%d] - %s",f.matricula,f.nome);
-					printf("\nSalario: %.2f",f.salario);
-				fread(&f,sizeof(tp_func),1,Ptr);
-			}
+
+void Cadastro(void)
+{
+	TpFunc R;
+	
+	FILE *PtrArq = fopen("func.dat","ab");
+	printf("\n### Cadastro de Funcionarios ###\n");
+	printf("Matricula: ");
+	scanf("%d",&R.Matricula);
+	while(R.Matricula>0)
+	{
+		printf("Nome: ");
+		fflush(stdin);
+		gets(R.Nome);
+		printf("Salario R$: ");
+		scanf("%f",&R.Salario);
+		fwrite(&R,sizeof(TpFunc),1,PtrArq);
+		printf("\n\nMatricula: ");
+		scanf("%d",&R.Matricula);
+	}
+	fclose(PtrArq);
+}
+
+void Exibir(void)
+{
+	TpFunc R;
+	FILE *Ptr = fopen("func.dat","rb");
+	if(Ptr==NULL)
+		printf("\nErro de Abertura!\n");
+	else
+	{
+		printf("\n### Conteudo do Arquivo ###\n");
+		fread(&R,sizeof(TpFunc),1,Ptr);
+		while(!feof(Ptr))
+		{
+			printf("\nMatricula [%d] - %s",R.Matricula,R.Nome);
+			printf("\nSalario: %.2f",R.Salario);
+			fread(&R,sizeof(TpFunc),1,Ptr);
+		}
 		fclose(Ptr);
-		}		
-getch();
-}
-char menu(void){
-clrscr();
-printf("\n # # #	 M E N U 	# # #\n");
-printf("\n[ A ] - Cadastro Funcionarios");
-printf("\n[ B ] - Exibir Funcionarios");
-printf("\n[ C ] - Consultar Funcionarios");
-printf("\n[ D ] - Ordenar Funcionarios");
-printf("\n[ESC] - Finalizar");
-printf("\nOpcao Desejada: \n");
-return toupper(getche());
+	}
+	getch();
 }
 
-int busca_cadastro(int matric,FILE *ptr){
-	tp_func r;
-	rewind(ptr);
-	
-	fread(&r,sizeof(tp_func),1,ptr); //entra lido
-	while(!feof(ptr) && matric!=r.matricula)
-		fread(&r,sizeof(tp_func),1,ptr); //sai lido
-	
-	if(!feof(ptr))
-		return -1;
-	else
-	 return 1;
-	
+char Menu(void)
+{
+	clrscr();
+	printf("\n# # # #   M E N U   # # # #\n");
+	printf("[A] - Cadastrar Funcionarios");
+	printf("\n[B] - Exibir Funcionarios");
+	printf("\n[C] - Consultar Funcionarios");
+	printf("\n[D] - Alterar Funcionarios");
+	printf("\n[E] - Ordenar Funcionarios");
+	printf("\n[ESC] - Finalizar");
+	printf("\nOpcao desejada: \n");
+	return toupper(getche());
+}
+
+void Consultar()
+{
+	TpFunc Reg;
+	int pos;
+	FILE *PtrFunc = fopen("func.dat","rb");
+	printf("\n### Consultar por Matricula ###\n");
+	printf("Digite a Matricula: ");
+	scanf("%d",&Reg.Matricula);
+	while (Reg.Matricula>0)
+	{
+		pos = BuscaFunc(PtrFunc,Reg.Matricula);
+		if (pos==-1)
+			printf("\nFuncionario nao Cadastrado!\n");
+		else
+			{
+				fseek(PtrFunc,pos,0);
+				fread(&Reg,sizeof(TpFunc),1,PtrFunc);
+				printf("\n*** Detalhes do Registro ***\n");
+				printf("Matricula: %d\n",Reg.Matricula);
+				printf("Nome: %s\n",Reg.Nome);
+				printf("Salario: R$ %.2f\n",Reg.Salario);
+				getch();
+			}
+		
+		printf("Digite a Matricula: ");
+		scanf("%d",&Reg.Matricula);
+	}
+	fclose(PtrFunc);
 }
 
 void Alterar()
 {
 	TpFunc Reg;
 	int pos;
-	FILE *PtrFunc= fopen("func.dat","rb+"); // Se eu quiser gravar em cima de um arquivo se usa rb+
-	printf("\n### Alterar por Matricula###\n");
-	printf("Digite a Matricula");
-	scanf("%d",&r.matricula);
-	
-	while(reg.matricula>0)
+	FILE *PtrFunc = fopen("func.dat","rb+");
+	printf("\n### Alterar por Matricula ###\n");
+	printf("Digite a Matricula: ");
+	scanf("%d",&Reg.Matricula);
+	while (Reg.Matricula>0)
 	{
-		pos = BuscaFunc(PtrFunc,reg.matricula);
-		if(pos==-1)
-			printf("\n Funcionario Não Cadastrado");
+		pos = BuscaFunc(PtrFunc,Reg.Matricula);
+		if (pos==-1)
+			printf("\nFuncionario nao Cadastrado!\n");
 		else
-		{
-			fseek(PtrFunc,pos,0); // fseek volta
-			fread(&r,sizeof(tp_func),i,PtrFunc)
-			printf("\n*** Detalhes Do Registro***\n");
-			printf("Matricula: %d\n",reg.matricula);
-			printf("Nome: %s\n",reg.Nome);
-			printf("Salario: R$ %.2f\n",reg.Salario);
-			
-			printf("\n Confirma Alteracao <S/N>\n");
-			if(toupper*getche())=='S')
 			{
-				printf("Novo Nome: ");
-				fflush(stdin);
-				gets(reg.Nome);
-				printf("Novo Salario R$: ");
-				scanf("%f",&reg.salario);
-				
 				fseek(PtrFunc,pos,0);
-				fwrite(&reg,sizeof(TpFunc),1,PtrFunc);
-				printf("\nRegistro Alterado!\n");
+				fread(&Reg,sizeof(TpFunc),1,PtrFunc);
+				printf("\n*** Detalhes do Registro ***\n");
+				printf("Matricula: %d\n",Reg.Matricula);
+				printf("Nome: %s\n",Reg.Nome);
+				printf("Salario: R$ %.2f\n",Reg.Salario);
+				
+				printf("\nConfirma Alteracao (S/N)? ");
+				if(toupper(getche())=='S')
+				{
+					printf("Novo Nome: ");
+					fflush(stdin);
+					gets(Reg.Nome);
+					printf("Novo Salario R$: ");
+					scanf("%f",&Reg.Salario);
+					
+					fseek(PtrFunc,pos,0);
+					fwrite(&Reg,sizeof(TpFunc),1,PtrFunc);
+					printf("\nRegistro Alterado!\n");
+				}
 			}
-		}
 		getch();
 		printf("\n\nDigite a Matricula: ");
-		scanf("%d",&reg.Matricula)
+		scanf("%d",&Reg.Matricula);
 	}
 	fclose(PtrFunc);
 }
 
-
-/*void Ordenar(void)   MUDE O CODIGO DE VETOR PARA ARQUIVO ( QUESTÃO DE PROVAAAA!!!!!! )
-{
-	int a,b;
-	for(a=0;a<TL-1;a++)
-		for(b=a+1;b<TL;b++)
-		{
-			if(V[a]>V[b])
-			{
-				aux= V[a];
-				V[a]= V[b];
-				V[b]=aux;
-			}
-		}
-}*/
-// EM ARQUIVO
-
 void Ordenar(void)
 {
-	int a, b;
+	int a, b, QtdeReg;
 	TpFunc RA, RB;
-	FILE * PtrFunc=fopepn("Func.dat","rb+");
-	if(PtrFunc==NULL)
-		printf("\nErro de Avertyra!\n");
+	FILE *PtrFunc = fopen("Func.dat","rb+");
+	if (PtrFunc==NULL)
+		printf("\nErro de Abertura!\n");
 	else
-	{
-		fseek(PtrFunc,0,2);
-		QtdeReg = ftell(PtrFunc)/sizeof(TpFunc);
-		for(a=0;a<QtdeReg-1;a++)
-			for(b=a+1;b<QtdeReg;b++)
-			{
-				fseek(PtrFunc,a*sizeof(TpFunc),0);
-				fread(&RA,sizeof(TpFunc),1,PtrFunc);
-				
-				fseek(PtrFunc,b*sizeof(TpFunc),0);
-				fread(&RB,sizeof(TpFunc),1,PtrFunc);
-				if(RA.matricula > RB.matricula)
+		{
+			fseek(PtrFunc,0,2);
+			QtdeReg = ftell(PtrFunc)/sizeof(TpFunc);
+			for(a=0; a<QtdeReg-1; a++)
+				for(b=a+1; b<QtdeReg; b++)
 				{
 					fseek(PtrFunc,a*sizeof(TpFunc),0);
-					fwrite(&RB,sizeof(TpFunc),1,PtrFunc);
+					fread(&RA,sizeof(TpFunc),1,PtrFunc);
 					
 					fseek(PtrFunc,b*sizeof(TpFunc),0);
-					fwrite(&RA,sizeof(TpFunc),1,PtrFunc);
+					fread(&RB,sizeof(TpFunc),1,PtrFunc);
 					
-				}
-			}
-	}
-}
-void cadastro(void){
-	tp_func r;
-	int pos;
-
-	
-	printf("\n*** cadastro de Funcionario ***\n");
-	FILE *Ptr=fopen("func.dat","wb");	
-	printf("\n Matricula: ");
-	scanf("%d",&r.matricula);
-
-pos=busca_cadastro(r.matricula,Ptr);
-
-if(pos==-1)
-	printf("Matricula ja existente!");	
-
- else{
-	while(r.matricula>0){
-			printf("Nome: ");
-			fflush(stdin);
-			gets(r.nome);
-			
-			printf("salario: ");
-			scanf("%f",&r.salario);
-			fwrite(&r,sizeof(tp_func),1,Ptr);
-				
-			printf("\n Matricula: ");
-			scanf("%d",&r.matricula);
-	}		
- }
- fclose(Ptr);
-}
-
-
-
-
-/*
-void busc_lugar(){
-	tp_func r;
-	FILE *ptr=fopen("func.dat","wb");
-	rewind(ptr);
-	int aux;
-	
-	int matric =r.matricula;
-		fread(&r,sizeof(tp_func),1,ptr); //entra lido
-	while(!feof(ptr) && r.matricula<=matric){
-		
-		
-		
-		aux=registro.matricula;
-		registro.matricula=matricula;
-		matricula=aux;
-		
-		
-		
-		fread(&r,sizeof(tp_func),1,ptr); //sai lido	++
-	}
-	fclose(ptr);
-}
-*/
-
-void ordenar(void){
-	int pos;
-	tp_func aux;
-	FILE *ptr=fopen("func.dat","wb");
-	rewind(ptr);
-
-	printf("### Ordenação de Funcionarios ###\n");
-	///	pos = busca_lugar();
-	printf("Ordenacao Completa!");
-	getch();
-	fclose(ptr);
-}
-
-
-void consultar(void){
-	int pos;
-	tp_func registro;
-	FILE *PtrFuncionario = fopen("func.dat","rb");
-
-	printf("\n ###	Consultar por Matricula	###\n\n");
-	printf("digite a matricula desejada: ");
-	scanf("%d",&registro.matricula);	///AUXiliar
-	while(registro.matricula>0){
-		
-		pos=BuscaFunc(PtrFuncionario,registro.matricula);
-		
-		if(pos==-1)
-			printf("\nFuncionario nao Cadastrado!\n");
-		else{
-			
-			fseek(PtrFuncionario,pos,0 /*ou SEEK_SET*/);///voltar o ponteiro (reposiciona o ponteiro) parametros: fseek(Ponteiro,deslocamento em bytes, a partir de onde o deslocamento)
-			fread(&registro,sizeof(tp_func),1,PtrFuncionario);//sempre usar para ajustar o ponteiro para leitura
-			printf("\n*** Detalhes do Registro ***\n\n");
-			printf("Matricula: [ %d ]\n",registro.matricula);
-			printf("Nome: %s \n",registro.nome);
-			printf("Salario: R$ %.2f\n\n",registro.salario);
-			
-		}
+					if(RA.Matricula > RB.Matricula)
+					{
+						fseek(PtrFunc,a*sizeof(TpFunc),0);
+						fwrite(&RB,sizeof(TpFunc),1,PtrFunc);
+						
+						fseek(PtrFunc,b*sizeof(TpFunc),0);
+						fwrite(&RA,sizeof(TpFunc),1,PtrFunc);
+					}
+				}	
+			printf("\nArquivo Ordenado!\n");
+			fclose(PtrFunc);
+		}		
 		getch();
-		printf("digite outra matricula desejada: \n");
-		scanf("%d",&registro.matricula);
-	}
-	fclose(PtrFuncionario);
 }
 
-int main(void){
-	char resp;
-	do{
-		switch(resp){
-			case 'A':cadastro();
-					 break;
-			case 'B':exibir();
-					 break;
-			case 'C':consultar();
-					 break;
-			case 'D':ordenar();
-					 break;			
+void Exclusao (void)
+{
+	TpFunc Reg;
+	int pos,Mat;
+	FILE *PtrFunc = fopen("func.dat","rb");
+	printf("\n### EXCLUSAO por FUNCIONARIO ###\n");
+	printf("Matricula a excluir: ");
+	scanf("%d",&Mat);
+	while (Mat>0)
+	{
+		pos = BuscaFunc(PtrFunc,Mat);
+		if (pos==-1)
+		{
+			printf("\nFuncionario nao Cadastrado!\n");
+			fclose(PtrFunc);
 		}
-		
-		resp=menu();
-	}while(resp!=27);
-return 0;
+		else
+			{
+				fseek(PtrFunc,pos,0);
+				fread(&Reg,sizeof(TpFunc),1,PtrFunc);
+				printf("\n*** Detalhes do Registro ***\n");
+				printf("Matricula: %d\n",Mat);
+				printf("Nome: %s\n",Reg.Nome);
+				printf("Salario: R$ %.2f\n",Reg.Salario);
+				
+				printf("\nConfirma Alteracao (S/N)? ");
+				if(toupper(getche())=='S')
+				{
+					FILE *PtrTemp = fopen("Temp.dat","wb")//CRIA UM NOVO ARQUIVO PARA REPASSAR OS DADOS DO ORIGINAL PARA REALIZAR A EXCLUSAO
+					fseek(PtrFunc,0,0);
+					fread(&Reg,sizeof(TpFunc),1,PtrFunc);
+					while (!feof(PtrFunc))
+					{
+						if(Reg.Matricula != Mat)
+							fwrite(&Reg,sizeof(TpFunc),1,PtrTemp)	// Vou gravar o arquivo original no novo para poder excluir ele ( Gravar menos o que n quer )
+							
+						fread(&Reg,sizeof(Tpfunc,1,PtrFunc));
+					}
+					// Tem que fechar ambos arquivos para conseguir excluir e renomear , se n não vai excluir e nem renomear
+					fclose(PtrFunc);
+					fclose(PtrTemp);
+					remove("func.dat"); // ESSE REMOVE ELE VAI EXCLUIR UM ARQUIVO INTEIRO
+					rename("Temp.dat","Func.dat"); // Esse rename é o nome atual e o nome que eu quero
+					printf("\nRegistro excluido com sucesso!");
+					getch();
+				}
+				else fclose(PtrFunc);
+			}
+		getch();
+		printf("\nMatricula a excluir: ");
+		scanf("%d",&Mat);
+		FILE * PtrFunc = fopen("Func.dat","rb");
+	}
+}
+	
+
+int main(void)
+{
+	char op;
+	do
+	{
+		op = Menu();
+		switch(op)
+		{
+			case 'A': Cadastro();
+					  break;
+					  
+			case 'B': Exibir();
+					  break;
+					  
+			case 'C': Consultar();
+					  break;
+					  
+			case 'D': Alterar();
+					  break;
+					  
+			case 'E': Ordenar();
+					  break;
+		}
+	}while (op!=27);
+	return 0;
 }
